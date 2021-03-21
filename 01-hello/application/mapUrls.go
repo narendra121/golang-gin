d3cb1df07de98bae1a7f3c2e7209fdaf7c9b22fb
narendra121 <narendra.yejarla123@gmail.com>
2021-03-21 18:06:32 +0530

    Using go and gin hello app

--- 01-hello/application/mapUrls.go
+++
@@ -1,7 +0,0 @@
-package application
-
-import "github.com/narendra121/golang-gin/01-hello/controller"
-
-func MapUrls() {
-	router.GET("/ping", controller.Hello)
-}
--- 01-hello/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/narendra121/golang-gin/01-hello/application"
-
-func main() {
-	application.Startapplication()
-}
